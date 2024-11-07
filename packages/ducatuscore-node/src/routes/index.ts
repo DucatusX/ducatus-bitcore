4bee97ccad990f92be93d9933bb5249f60792841
nezort11 <nezort11@gmail.com>
2024-11-07 13:18:08 +0300

    fix: Fix TS6133 import rate limiter is declared but never used

--- packages/ducatuscore-node/src/routes/index.ts
+++ packages/ducatuscore-node/src/routes/index.ts
@@ -3,7 +3,12 @@ import { Request, Response } from 'express';
 import express from 'express';
 import config from '../config';
 import { Config } from '../services/config';
-import { CacheMiddleware, CacheTimes, LogMiddleware, RateLimiter } from './middleware';
+import {
+  CacheMiddleware,
+  CacheTimes,
+  LogMiddleware
+  // RateLimiter
+} from './middleware';
 import { Web3Proxy } from './web3';
 
 const app = express();
