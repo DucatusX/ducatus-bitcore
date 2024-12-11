ed3eaa41adb536431e1d6bde21ec74a08968b551
Terlibin Oleg <rumblefishinfo@gmail.com>
2024-12-11 16:40:24 +0300

    Revert: update packages

--- packages/ducatuscore-wallet-service/src/app.ts
+++ packages/ducatuscore-wallet-service/src/app.ts
@@ -4,7 +4,7 @@ var spawn = require('child_process').spawn;
 var async = require('async');
 
 var scripts = [
-  // 'locker/locker.js',
+  'locker/locker.js',
   'messagebroker/messagebroker.js',
   'bcmonitor/bcmonitor.js',
   'emailservice/emailservice.js',
@@ -16,7 +16,7 @@ var scripts = [
 async.eachSeries(scripts, function(script, callback) {
   console.log(`Spawning ${script}`);
 
-  const node = spawn('node', [`${__dirname}/` + script]);
+  const node = spawn('node', [script]);
   node.stdout.on('data', data => {
     console.log(`${data}`);
   });
