4c604af88f5823661f7c773e5a76a88c1fe394b8
Tavern_Master <dimasamusch@yandex.ru>
2024-12-24 14:41:30 +0300

    fix: add auto-restart scripts in dws app.js

--- packages/ducatuscore-wallet-service/src/app.ts
+++ packages/ducatuscore-wallet-service/src/app.ts
@@ -16,13 +16,39 @@ if (process.env.MODE === 'dev') {
   });  
 } else {
   async.eachSeries(scripts, function(script, callback) {
-    const node = spawn('node', [`${__dirname}/` + script]);
-    const scriptName = script.split('/')[0].split('.')[0]
-    node.stdout.on('data', data => console.log(`\x1b[35m${scriptName} \x1b[0m| ${data}`));
-  
-    node.stderr.on('data', data => console.error(`\x1b[35m${scriptName} \x1b[0m| ${data}`));
-  
-    callback();
+    runScript(script)
+    callback()
   });
 }
 
+function runScript(script) {
+  const scriptPath = `${__dirname}/${script}`;
+  const scriptName = script.split('/')[0].split('.')[0];
+
+  const node = spawn('node', [scriptPath]);
+  console.log(`\x1b[33m[INFO]\x1b[0m Started script: ${scriptName} (PID: ${node.pid})`);
+
+
+  node.stdout.on('data', data => console.log(`\x1b[35m${scriptName} \x1b[0m| ${data}`));
+  node.stderr.on('data', data => console.error(`\x1b[31m${scriptName} \x1b[0m| ${data}`));
+
+
+  node.on('close', (code) => {
+     if (code === 0) {
+        console.log(`\x1b[32m[INFO]\x1b[0m Script ${scriptName} exited successfully with code ${code}.`);
+     } else {
+       console.error(`\x1b[31m[ERROR]\x1b[0m Script ${scriptName} exited with code ${code}. Restarting...`);
+        setTimeout(() => {
+            runScript(script)
+        }, 1000) // Restart after a 1-second delay
+     }
+  });
+
+
+  node.on('error', (err) => {
+    console.error(`\x1b[31m[ERROR]\x1b[0m Failed to spawn script ${scriptName}:`, err);
+        setTimeout(() => {
+             runScript(script)
+           }, 1000)
+  });
+}
